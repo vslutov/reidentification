7da7bc189d34137326f86ad9b540193b1c291b1d
vslutov <vslutov@yandex.ru>
2017-05-08 14:23:35 +0300

    Revert "Evaluate pca precision"
    This reverts commit 5364af237b3bf5d324c814f67cddd94ba70cb943.

--- reidentification/evaluate.py
+++ reidentification/evaluate.py
@@ -7,10 +7,8 @@
 from tabulate import tabulate
 import numpy as np
 np.random.seed(123)
-from sklearn.decomposition import PCA
-from sklearn import neighbors
 
-from .models import models, ModelType, normalize_images
+from .models import models, ModelType
 from .datasets import datasets, DatasetType
 
 def evaluate(args):
@@ -27,29 +25,10 @@ def evaluate(args):
                                        y_train=dataset['y_train'],
                                       )
 
-    N_NEIGHBORS = 5
-    metric_names = ['rank-1', 'rank-{n_neighbors}'.format(n_neighbors=N_NEIGHBORS)]
+    classifier = models[args.classifier].prepare(indexator=model.get_indexator(),
+                                                 X_test=dataset['X_test'],
+                                                 y_test=dataset['y_test'],
+                                                )
 
-    indexator = model.get_indexator()
-    X_test = indexator.predict(normalize_images(dataset['X_test']))
-    X_query = indexator.predict(normalize_images(dataset['X_query']))
-    y_test=dataset['y_test']
-    y_query=dataset['y_query']
-
-    for i in range(16, 256, 8):
-        pca = PCA(n_components=i)
-        pca.fit(X_test)
-        X_base = pca.transform(X_test) > 0
-        X_find = pca.transform(X_query) > 0
-        classifier = neighbors.NearestNeighbors(n_neighbors=N_NEIGHBORS, metric='l1', n_jobs=-1)
-        classifier.fit(X_base)
-        our_neighbours = classifier.kneighbors(X_find, return_distance=False)
-        y_pred = y_test[our_neighbours]
-        y_true = np.hstack([np.array(y_query).reshape((-1, 1))] * N_NEIGHBORS)
-        positive = y_pred == y_true
-        precisions = (positive[:, 1].sum() / y_query.size,
-                      positive.max(axis=1).sum() / y_query.size,
-                     )
-        with open('log.txt', 'a') as logfile:
-            print(i, precisions, file=logfile)
-        print(i, precisions)
+    print(tabulate([classifier.evaluate(dataset['X_query'], dataset['y_query'])],
+                   headers=classifier.metric_names))
